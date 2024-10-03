519680bf0db13339804523826216b2be710bde5b
AngelvRios <128487739+AngelvRios@users.noreply.github.com>
2024-10-03 17:55:14 -0300

    a

---
+++ src/components/pop-up.jsx
@@ -0,0 +1,14 @@
+import { Popover, Text, Button } from '@mantine/core';
+
+function Demo() {
+  return (
+    <Popover width={200} position="bottom" withArrow shadow="md">
+      <Popover.Target>
+        <Button>Toggle popover</Button>
+      </Popover.Target>
+      <Popover.Dropdown>
+        <Text size="xs">This is uncontrolled popover, it is opened when button is clicked</Text>
+      </Popover.Dropdown>
+    </Popover>
+  );
+}
