24fd51a4a685aa3ca9d14ce5536a8c7307e9e833
Denisse Maldonado <128488596+usagikitty@users.noreply.github.com>
2024-10-09 19:11:49 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,9 @@
-import "@mantine/core/styles.css";
-import { useState } from "react";
-import { MantineProvider, Container, Group, BackgroundImage, Box } from "@mantine/core";
-import ComboBoxFilter from "./components/combobox";
-import Titulo from "./components/title";
-import CarruselAnimales from "./components/carouselAnimals";
-import CarruselPerros from "./components/carouselDogs";
-import CarruselGatos from "./components/carouselCats";
-import CarruselConejos from "./components/carouselRabbit";
-import CarruselRoedores from "./components/carouselMouse";
-import CarruselAves from "./components/carouselBird";
-import Detalles from "./components/pop-up";
+import TodoApp from "./components/TodoApp"
 
-export default function App() {
-  const [selectedRegionId, setSelectedRegionId] = useState(0);
-  const carruselAnimales = <CarruselAnimales selectedRegionId={selectedRegionId}/>;
-  const carruselPerros = <CarruselPerros selectedRegionId={selectedRegionId}/>;
-  const carruselGatos = <CarruselGatos selectedRegionId={selectedRegionId}/>;
-  const carruselConejos = <CarruselConejos selectedRegionId={selectedRegionId}/>;
-  const carruselRoedores = <CarruselRoedores selectedRegionId={selectedRegionId}/>;
-  const carruselAves = <CarruselAves selectedRegionId={selectedRegionId}/>;
-
-  const areAllCarouselsEmpty = 
-    !carruselAnimales &&
-    !carruselPerros &&
-    !carruselGatos &&
-    !carruselConejos &&
-    !carruselRoedores &&
-    !carruselAves;
-
+function App() {
   return (
-    <MantineProvider>
-      <Box style={{ width: '100vw', height: '100vh' }}>
-            <BackgroundImage
-              src="https://img.freepik.com/vector-gratis/adorable-patron-huella-animal-diseno-fondo_1017-53348.jpg"
-              radius="sm"
-              style={{minHeight: '100vh',
-                backgroundPosition: 'center',
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat',
-                }}
-            >
-              <Container>
-                <Group gap="xl">
-                <ComboBoxFilter onRegionChange={setSelectedRegionId}/>
-                <Titulo />
-                
-                </Group>
-                <Detalles />
-                <CarruselAnimales selectedRegionId={selectedRegionId}/>
-                <CarruselPerros selectedRegionId={selectedRegionId}/>
-                <CarruselGatos selectedRegionId={selectedRegionId}/>
-                <CarruselConejos selectedRegionId={selectedRegionId}/>
-                <CarruselRoedores selectedRegionId={selectedRegionId}/>
-                <CarruselAves selectedRegionId={selectedRegionId}/>
-
-              </Container>
-            </BackgroundImage>
-      </Box>
-    </MantineProvider>
-  );
+    <TodoApp />
+  )
 }
+
+export default App
