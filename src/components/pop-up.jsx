1cade04fa410129e9ceec6e764059acef78c5f76
Denisse Maldonado <128488596+usagikitty@users.noreply.github.com>
2024-10-09 01:51:56 -0300

    Add files via upload

--- src/components/pop-up.jsx
+++ src/components/pop-up.jsx
@@ -1,15 +1,34 @@
-import { Popover, Text, Button } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { Modal, Button } from '@mantine/core';
 
-function PopUp() {
+function Detalles() {
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
-    <Popover width={200} position="bottom" withArrow shadow="md">
-      <Popover.Target>
-        <Button>Toggle popover</Button>
-      </Popover.Target>
-      <Popover.Dropdown>
-        <Text size="xs">This is uncontrolled popover, it is opened when button is clicked</Text>
-      </Popover.Dropdown>
-    </Popover>
+    <>
+  <Modal
+        opened={opened}
+        onClose={close}
+        title="Nombre del animal"
+>
+  <div style={{
+    backgroundColor: 'rgba(45, 214, 247, 1)', 
+    border: '15px solid yellow',
+    borderRadius: '10px',
+    padding: '20px',
+    }}>
+      <img 
+        src="https://via.placeholder.com/150" 
+        alt="Animal" 
+        style={{ width: '100%', height: '50%', borderRadius: '10px', marginBottom: '20px' }} 
+        />
+        <p>Aquí va la descripción del animal.</p>
+      </div>
+    </Modal>
+
+      <Button onClick={open}>Open modal</Button>
+    </>
   );
 }
-export default PopUp;
+
+export default Detalles;
